3859e0f47ece9136b4a808eb8b5a6e04d15353a8
Samuel Ganta <samuelganta@gmail.com>
2018-06-04 10:53:26 -0500

    .

--- src/test/java/com/in28minutes/unittesting/unittesting/business/SomeBusinessTest.java
+++ src/test/java/com/in28minutes/unittesting/unittesting/business/SomeBusinessTest.java
@@ -12,7 +12,7 @@ public class SomeBusinessTest {
 		SomeBusinessImpl business = new SomeBusinessImpl();
 		int actual = business.calculateSum(new int[] {1,2,3});
 		int expected = 6; 
-		assertEquals(expected,actual);
+		assertEquals(expected,actual); 
 	}
 	
 	@Test
